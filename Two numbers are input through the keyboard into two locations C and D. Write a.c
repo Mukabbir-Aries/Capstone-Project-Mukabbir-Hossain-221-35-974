ce57590807f81e8c0db8023394573c0db3868ff2
Mukabbir-Aries <101591994+Mukabbir-Aries@users.noreply.github.com>
2022-06-10 23:51:11 +0600

    Add files via upload

---
+++ Two numbers are input through the keyboard into two locations C and D. Write a.c
@@ -0,0 +1,19 @@
+//Mukabbir Aries//
+#include<Stdio.h>
+void main()
+{
+    int C, D;
+     printf("Enter the value of C: ");
+     scanf("%d", &C);
+     printf("Enter the value of D: ");
+     scanf("%d", &D);
+
+     C = C+D;
+
+     D = C-D;
+
+     C = C-D;
+     printf("\n The value of C: %d", C);
+     printf("\n The value of D: %d", D);
+     getch();
+}

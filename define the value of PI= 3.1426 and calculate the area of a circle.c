70bd365890beb89836370e08992657bcab92d672
Mukabbir-Aries <101591994+Mukabbir-Aries@users.noreply.github.com>
2022-06-11 01:10:15 +0600

    Add files via upload

---
+++ define the value of PI= 3.1426 and calculate the area of a circle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+printf("Take Radious value to calculate the Circle area : ");
+float Pi=3.1416,radious,circle_area;
+scanf("%f",&radious);
+circle_area=Pi*radious*radious;
+printf("circle area is :\n%f",circle_area);
+}

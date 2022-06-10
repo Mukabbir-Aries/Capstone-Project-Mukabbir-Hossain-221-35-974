549c610403024c2c35175d69f92bd01240694eae
Mukabbir-Aries <101591994+Mukabbir-Aries@users.noreply.github.com>
2022-06-10 23:38:55 +0600

    Add files via upload

---
+++ program to convert this temperature into Centigrade degrees..c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+printf("Enter celcius vallue : ");
+int celcius ,fahrenheit;
+scanf("%d",&celcius);
+//we know fahrenheit=(celcius*9/5)+32//
+fahrenheit=(celcius*9/5)+32;
+printf("%d\n",fahrenheit);
+
+}
+

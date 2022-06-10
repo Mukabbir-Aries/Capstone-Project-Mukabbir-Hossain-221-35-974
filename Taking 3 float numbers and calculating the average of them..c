72acea7ce48cc00237a4b7f19146c6ecbbfd0857
Mukabbir-Aries <101591994+Mukabbir-Aries@users.noreply.github.com>
2022-06-10 23:24:55 +0600

    Add files via upload

---
+++ Taking 3 float numbers and calculating the average of them..c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+float num1,num2,num3,t,g;
+printf("Take the numbers:");
+scanf("%f %f %f",&num1,&num2,&num3);
+t=(num1+num2+num3)/3;
+printf("\Avarage the number is:%f",t);
+return 0;
+}

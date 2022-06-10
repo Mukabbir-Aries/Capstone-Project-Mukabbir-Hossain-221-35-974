c5a4a8179e84386844d786c1d8fb48f7c20a8004
Mukabbir-Aries <101591994+Mukabbir-Aries@users.noreply.github.com>
2022-06-11 03:40:04 +0600

    Add files via upload

---
+++ check whether a number is even or odd using if else..c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+printf(" ^_^ Enter a Number ^_^: ");
+int a;
+scanf("%d",&a);
+if(a%2==0)
+    printf("%d is a even number",a);
+else
+    printf("%d is a odd number",a);
+}

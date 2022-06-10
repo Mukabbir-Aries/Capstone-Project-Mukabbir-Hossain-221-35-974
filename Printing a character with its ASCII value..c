7c69df902138587186788267d8dc3b4b5726ca64
Mukabbir-Aries <101591994+Mukabbir-Aries@users.noreply.github.com>
2022-06-11 00:57:50 +0600

    Add files via upload

---
+++ Printing a character with its ASCII value..c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char c;
+    printf("**Enter a character from keyboard**: ");
+    scanf("%c", &c);
+
+    printf("ASCII value of %c = %d", c,c);
+
+    return 0;
+}
